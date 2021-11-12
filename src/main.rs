054cc9c95ab0b0c69c2337cd8ee4e434e9eff1ed
Mikolaj Figurski <meek.f80@gmail.com>
2021-11-12 18:57:19 -0500

    Download image

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::env;
+extern crate reqwest;
+use std::fs::File;
+use std::{env, io};
 
 fn main() {
     let args = (env::args().collect::<Vec<String>>()[1..]).to_vec();
-    println!("{}", format_url((1920, 1080), &args));
+    get_image(format_url((1920, 1080), &args), "./image.jpg");
+    // println!("{}", format_url((1920, 1080), &args));
 }
 
 fn format_url(size: (u16, u16), keywords: &Vec<String>) -> String {
@@ -12,4 +15,11 @@ fn format_url(size: (u16, u16), keywords: &Vec<String>) -> String {
         size.1,
         keywords.join(",")
     );
+}
+
+fn get_image(url: String, file_name: &str) {
+    let mut resp = reqwest::blocking::get(url).expect("request failed");
+    let mut out = File::create(file_name).expect("file creation failed");
+    io::copy(&mut resp, &mut out).expect("file copy failed");
+    // println!("{:?}", resp);
 }
