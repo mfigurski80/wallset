27c7194d272127df9d0b898200e984596a183580
Mikolaj Figurski <meek.f80@gmail.com>
2021-12-25 21:15:07 -0500

    Add fix for my own environment

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,16 @@ fn main() {
     let args = (env::args().collect::<Vec<String>>()[1..]).to_vec();
     let path_to_write = "/home/miko/Pictures/Wallpapers/auto.jpg";
     get_image(format_url((1920, 1080), &args), path_to_write);
-    set_wallpaper(path_to_write);
+    // set_wallpaper(path_to_write);
 }
 
-fn set_wallpaper(path: &str) {
-    let orig = wallpaper::get().unwrap();
-    println!("Original paper: {:?}", orig);
-    wallpaper::set_from_path(path).expect("wallpaper set failed");
-    wallpaper::set_mode(wallpaper::Mode::Crop).expect("wallpaper mode set failed");
-    println!("New wallpaper:  {:?}", wallpaper::get().unwrap());
-}
+// fn set_wallpaper(path: &str) {
+//     let orig = wallpaper::get().unwrap();
+//     println!("Original paper: {:?}", orig);
+//     wallpaper::set_from_path(path).expect("wallpaper set failed");
+//     wallpaper::set_mode(wallpaper::Mode::Crop).expect("wallpaper mode set failed");
+//     println!("New wallpaper:  {:?}", wallpaper::get().unwrap());
+// }
 
 fn format_url(size: (u16, u16), keywords: &Vec<String>) -> String {
     return format!(
